9890a1da40038d2589ca7329d46eb7058fea2887
Ngo Xuan Thiep <ngothiep2412@gmail.com>
2024-10-28 20:18:22 +0700

    feature(cinema): update assets

--- lib/Cinema App UI/pages/home_page_cinema.dart
+++ lib/Cinema App UI/pages/home_page_cinema.dart
@@ -1,14 +1,132 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ui_design/Cinema%20App%20UI/constants/constants.dart';
+import 'package:flutter_ui_design/Cinema%20App%20UI/models/category_model.dart';
 
 class HomePageCinema extends StatelessWidget {
   const HomePageCinema({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: appBackgroundColor,
-      appBar: _AppBar(),
+      appBar: const _AppBar(),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 35,
+          ),
+          const _Search(),
+          const SizedBox(
+            height: 30,
+          ),
+          const Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Category',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 17,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'See All',
+                          style: TextStyle(
+                            color: buttonColor,
+                            fontSize: 13,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          size: 15,
+                          color: buttonColor,
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 17,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: List.generate(
+              categories.length,
+              (index) => Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      color: Colors.white10.withOpacity(0.05),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Image.asset(
+                      categories[index].emoji,
+                      fit: BoxFit.cover,
+                      width: 30,
+                      height: 30,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    categories[index].name,
+                    style: const TextStyle(
+                      fontSize: 14,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _Search extends StatelessWidget {
+  const _Search();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.symmetric(vertical: 17),
+          filled: true,
+          fillColor: Colors.white.withOpacity(.05),
+          hintText: "Search",
+          hintStyle: const TextStyle(
+            color: Colors.white54,
+          ),
+          prefixIcon: const Icon(
+            Icons.search,
+            size: 32,
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(27),
+            borderSide: BorderSide.none,
+          ),
+        ),
+      ),
     );
   }
 }
@@ -19,6 +137,7 @@ class _AppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      backgroundColor: appBackgroundColor,
       title: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
         child: Row(
